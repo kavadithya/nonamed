180f3dad7cb0f6efc2eebdf65fb17cf5794273ed
Adithya <kav.adithya@gmail.com>
2012-11-26 20:29:04 +0530

    working users signup and signin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ashwin::Application.config.secret_token = '93830a1d80741e591a7d066102d48a5ddbdafc794e7e85523caa75bbc2a4f417008362d9c7576195b510580069fb9d11f048b37b95070fda8055473289dac293'
